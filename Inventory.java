546f7ec81d7555da80ee04d64d1d3ff42bdc153b
Jacob London <jelondon12@gmail.com>
2016-02-03 22:58:40 -0800

    fix stats class worked on inventory class

---
+++ Inventory.java
@@ -0,0 +1,23 @@
+package gamepack;
+
+public class Inventory {
+	
+	private Potion[] potionArray;
+	private Enchantment[] enchantmentArray;
+	private Weapon weapon;
+	
+	public Inventory(Potion[] potionArray,Enchantment[] enchantmentArray,Weapon weapon){
+		this.potionArray=potionArray;
+		this.enchantmentArray=enchantmentArray;
+		this.weapon=weapon;
+	}
+	
+	public void getPotionArray(){
+		return potionArray;
+	}
+	public Potion[] setPotionArray(Potion[] potionArray){
+		this.
+	}
+	
+	
+}
--- Stats.java
+++ Stats.java
@@ -1,161 +1,194 @@
 package gamepack;
 
 
-public abstract class Potion extends Item{
+//stats for Character, Attackable
 
-	//blank Potion, expected override
-	public Potion(){}; 
+public class Stats {
+	
+	//the amount of health the Character has
+	private int health;
 
-	private int amount;
-	
-	private String name;
-	public Potion (int amount, String name){
+	//the amount of fatigue the Character has
+	private int fatigue;
 
-		this.amount = amount;
-		this.name = name;
+	//the amount of speed the Character has
+	private int speed;
+
+	//the evade chance the Character has
+	private double evade;
+
+	//the amount of experience the Character has
+	private int experience;
+
+	//the amount of base damage the Character has
+	private int damage;
+
+	//the amount of base critical hit chance the Character has
+	private double critChance;
+
+	//the amount of base critical hit damage the Character has
+	private double critDamage;
+
+	
+	public Stats(int health, int fatigue, int speed, double evade, int experience, int damage, double critChance, double critDamage) {
+		
+		this.health = health;
+		this.fatigue = fatigue;
+		this.speed = speed;
+		this. evade = evade;
+		this.experience =  experience;
+		this.damage = damage;
+		this.critChance = critChance;
+		this.critDamage = critDamage;
+	
 	}
-//toString
-	public String toString(){
-		return amount+" "+name+";
+
+	public void setHealth(int health){
+		this.health=health;
+	}
+	public int getHealth(){
+		return health;
+	}
+
+	public void setFatigue(int fatigue){
+		this.fatigue=fatigue;
+	}
+	public int getFatigue(){
+		return fatigue;
+	}
+
+	public void setSpeed(int speed){
+		this.speed=speed;
+	}
+	public int getSpeed(){
+		return speed;
+	}
+
+	public void setEvade(double evade){
+		this.evade=evade;
+	}
+	public double getEvade(){
+		return evade;
+	}
+
+	public void setExperience(int experience){
+		this.experience=experience;
+	}
+	public int getExperience(){
+		return experience;
+	}
+
+	public void setDamage(int damage){
+		this.damage=damage;
+	}
+	public int getDamage(){
+		return damage;
+	}
+
+	public void setCritChance(double critChance){
+		this.critChance=critChance;
+	}
+	public double getCritChance(){
+		return critChance;
+	}
+
+	public void setCritDamage(double critDamage){
+		this.critDamage=critDamage;
+	}
+	public double getCritDamage(){
+		return critDamage;
+	}
+}
+
+
+class playerStats extends Stats {
+	
+	//Modifiers for statuses
+	
+		//divides incoming damage
+		private static final double REDUCE_DAMAGE_TAKEN = 2;
+
+		//multiplies damage given
+		private static final double INCREASE_DAMAGE_GIVEN = 2;
+
+		//multiplies speed
+		private static final double SPEED_INCREASE = 1.5;
+
+//multiplies evade
+		private static final double EVADE_INCREASE = 1.5;
+		
+		//multiplies critical hit chance
+		private static final double CRIT_CHANCE_INCREASE = 2;
+
+		//multiplies critical hit damage
+		private static final double CRIT_DAMAGE_INCREASE = 2;
+	
+		//multiplies for percentage of health to gain
+		private static final double HEALTH_REGEN_AMOUNT = 1.05;
+
+		//adds for fatigue to regenerate
+		private static final int FATIGUE_REGEN_AMOUNT= 5;
+
+		//multiplies experience gained
+		private final double INCREASE_EXPERIENCE_GAINED = 2;
+
+	public playerStats(int health, int fatigue, int speed, double evade, int experience) {
+		
+		super(health,fatigue,speed,evade,experience);
+		
 	}
 	
-//amount	
-	public int getAmount(){
-		return amount;
+	//parameter use
+	public void takeReducedDamage(int damage){
+		if( status.reduceDamage() )
+			setHealth( getHealth() - (int)( damage / REDUCE_DAMAGE_TAKEN ) );	
+		
 	}
-	public void setAmount(int amountInput){
-		deltaHealth = amountInput;
+
+	public int giveIncreasedDamage(){
+		if( status.increaseDamage() )
+			return  (int)(getDamage() * INCREASE_DAMAGE_GIVEN);
 	}
-//name	
-	public String getName(){
-		return name;
+
+	public int increaseSpeed(){
+		if( status.increaseSpeed() )
+			return (int)(getSpeed() * SPEED_INCREASE);
 	}
-	public void setName(String nameInput){
-		name = nameInput;
+
+	public double increaseEvade(){
+		if( status.increaseEvade() )
+			return getEvade() * EVADE_INCREASE;
 	}
 	
-//obtain potion  increments the amount of potions owned by the type named by the amount given
-	public static Potion obtainPotion(Potion pickUp, Potion inInventory){
-		if(pickUp.getName().equals(inInventory.getName())){
-			inInventory.setAmount( pickUp.getAmount()+inInventory.getAmount() );
+	public double critChanceIncrease(){
+		if( status.critChanceIncrease() )
+			return getCritChance() * CRIT_CHANCE_INCREASE;
+	}
+
+	public double critDamageIncrease(){
+		if( status.critDamageIncreaes() )
+			return getCritDamage() * CRIT_DAMAGE_INCREASE;
+	}
+
+	public void healthRegen(){
+		if( status.healthRegen() )
+			setHealth( getHealth * HEALTH_REGEN_AMOUNT;
+	}
+
+	public void fatigueRegen(){
+		if( status.fatigueRegen() ){
+			if( getFatigue() + FATIGUE_REGEN_AMOUNT >100){
+				setFatigue(100);
+			}
+			else{
+				setFatigue( getFatigue() + FATIGUE_REGEN_AMOUNT);
+			}
 		}
-		return inInventory;
-	}
-//decrement potion amount in relation to potion, not stats	
-	public Potion usePotion(Potion inInventory){
-		inInventory.setAmount(inInventory.getAmount()-1);
-		return inInventory;
+			
 	}
 
-	public abstract void inflictStatus();
-}
-
-//each potion type will have its own subclass
-
-class damageTakenReductionPotion extends Potion {
-	
-	public damageTakenReductionPotion(int amount, String name){
-		super(amount,name);
+	public int increaseExperience(int experience){
+		if( status.increaseExperience() )
+			setExperience( getExperience() + (int)( experience*INCREASE_EXPERIENCE_GAINED ) );
 	}
-	
-	public void inflictStatus(Player player){
-		
-	}
-
-}
-
-class speedIncreasePotion extends Potion {
-	
-	public speedIncreasePotion(int amount, String name){
-		super(amount,name);
-	}
-	
-	public void inflictStatus(Player player){
-		
-	}
-
-}
-
-class critChanceIncreasePotion extends Potion {
-	
-	public critChanceIncreasePotion(int amount, String name){
-		super(amount,name);
-	}
-	
-	public void inflictStatus(Player player){
-		
-	}
-
-}
-
-class healthRegenPotion extends Potion {
-	
-	public healthRegenPotion(int amount, String name){
-		super(amount,name);
-	}
-	
-	public void inflictStatus(Player player){
-		
-	}
-
-}
-
-class evadeIncreasePotion extends Potion {
-	
-	public evadeIncreasePotion(int amount, String name){
-		super(amount,name);
-	}
-	
-	public void inflictStatus(Player player){
-		
-	}
-
-}
-
-class damageGivenIncreasePotion extends Potion {
-	
-	public damageGivenIncreasePotion(int amount, String name){
-		super(amount,name);
-	}
-	
-	public void inflictStatus(Player player){
-		
-	}
-
-}
-
-class critDamageIncreasePotion extends Potion {
-	
-	public critDamageIncreasePotion(int amount, String name){
-		super(amount,name);
-	}
-	
-	public void inflictStatus(Player player){
-		
-	}
-
-}
-
-class fatigueRegenPotion extends Potion {
-	
-	public fatigueRegenPotion(int amount, String name){
-		super(amount,name);
-	}
-	
-	public void inflictStatus(Player player){
-		
-	}
-
-}
-
-class doubleExperiencePotion extends Potion {
-	
-	public doubleExperiencePotion(int amount, String name){
-		super(amount,name);
-	}
-	
-	public void inflictStatus(Player player){
-		
-	}
-
 }
