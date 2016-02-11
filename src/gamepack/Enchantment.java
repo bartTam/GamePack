07fa052c3469c5817bb514ce051b12630803c7fa
Jacob London <jelondon12@gmail.com>
2016-02-10 22:24:38 -0800

    test commit with push

--- src/gamepack/Enchantment.java
+++ src/gamepack/Enchantment.java
@@ -1,21 +1,26 @@
 package gamepack;
 
-public class Enchantment{
+public abstract class Enchantment{
 	
-	//always false, unless activated by player, reset to false after battle ends
-	private boolean enchantmentIsActive = false;
+		//always false, unless activated by player, reset to false after battle ends
+		private boolean enchantmentIsActive = false;
 	
-	//the damage multiplier the Player has for the duration of the battle
-	private final int STRENGTH_ENCHANTMENT = 2;
+		//the amount of enchantments by type, by default 0
+		private int enchantmentAmount = 0;
+		
+		
+		//the damage multiplier the Player has for the duration of the battle
+		protected final int STRENGTH_ENCHANTMENT = 2;
 	
-	//the amount of accuracy added to the player's base critical chance;
-	private final double ACCURACY_ENCHANTMENT = 30;
+		//the amount of accuracy added to the player's base critical chance;
+		protected final int ACCURACY_ENCHANTMENT = 30;
 	
-	//the amount of critical chance added to the player's base critical chance
-	private final int CRITICAL_CHANCE_ENCHANTMENT = 25;
+		//the amount of critical chance added to the player's base critical chance
+		protected final int CRITICAL_CHANCE_ENCHANTMENT = 25;
 	
-	//the amount of fatigue that will be subtracted from use after each attack
-	private final int FATIGUE_ENCHANTMENT = 1;
+		//the amount of fatigue that will be subtracted from use after each attack
+		protected final int FATIGUE_ENCHANTMENT = 1;
+	
 	
 	public Enchantment(){
 		
@@ -24,25 +29,62 @@ public class Enchantment{
 	public void setEnchantmentIsActive(boolean set){
 		enchantmentIsActive = set;
 	}
-	
 	public boolean getEnchantmentIsActive(){
 		return enchantmentIsActive;
 	}
 	
-	public int getStrengthEnchantment(){
+	public void setEnchantmentAmount(int amount){
+		this.enchantmentAmount = amount;
+	}
+	public int getEnchantmentAmount(){
+		return enchantmentAmount;
+	}
+	
+	public void useEnchantment(){
+		if(enchantmentAmount > 0)
+			enchantmentAmount--;
+	}
+	
+	
+	
+	public abstract int getEnchantment();
+	
+}
+
+class StrengthEnchantment extends Enchantment{
+	
+	public StrengthEnchantment(){
+
+	}
+	
+	public int getEnchantment(){
 		return STRENGTH_ENCHANTMENT;
 	}
-	
-	public double getAccuracyEnchantment(){
+}
+
+class AccuracyEnchantment extends Enchantment{
+	public AccuracyEnchantment(){
+
+	}
+	public int getEnchantment(){
 		return ACCURACY_ENCHANTMENT;
 	}
-	
-	public int getCriticalChanceEnchantment(){
+}
+
+class CriticalChanceEnchantment extends Enchantment{
+	public CriticalChanceEnchantment(){
+
+	}
+	public int getEnchantment(){
 		return CRITICAL_CHANCE_ENCHANTMENT;
 	}
-	
-	public int getFatigueEnchantment(){
+}
+
+class FatigueEnchantment extends Enchantment{
+	public FatigueEnchantment(){
+
+	}
+	public int getEnchantment(){
 		return FATIGUE_ENCHANTMENT;
 	}
-	
 }
